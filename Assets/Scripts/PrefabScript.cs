344c33e3f43f221260589d498bbe34ede9e96668
gamewatabou <game.watabou@gmail.com>
2021-03-14 15:04:31 +0900

    プレハブが画面外に出たら消えるように、プレハブにItemタグを設定、Itemタグの個数をカウント

---
+++ Assets/Scripts/PrefabScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!GetComponent<Renderer>().isVisible)
+        {
+            int count = GameObject.FindGameObjectsWithTag("Item").Length;
+            Debug.Log(count);
+
+            Destroy(this.gameObject);
+        }
+    }
+}
